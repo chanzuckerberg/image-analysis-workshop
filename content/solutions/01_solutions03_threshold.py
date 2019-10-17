f80dfe31f31e14a8c6ee0e46968cdaf51cd2ce8c
sofroniewn <sofroniewn@gmail.com>
2019-10-16 20:35:43 -0500

    update

---
+++ content/solutions/01_solutions03_threshold.py
@@ -0,0 +1,4 @@
+# Your solution goes here!
+filters.try_all_threshold(plane_denoised)
+
+print(filters.threshold_isodata(plane_denoised))  # This one is for free!
---
+++ content/solutions/01_solutions04_regionprops.py
@@ -0,0 +1,5 @@
+plane_centroid = [prop.centroid for prop in properties]
+print('Centroids: {}'.format(plane_centroid))
+
+plane_equivalent_diameter = [prop.equivalent_diameter for prop in properties]
+print('Diameters: {}'.format(plane_equivalent_diameter))
